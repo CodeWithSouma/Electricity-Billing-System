aab23b51b18c43af2adc4a51099c0f3ac8a7cda8
Souma <souma.hitech@gmail.com>
2021-01-02 23:59:30 +0530

    complete connection class

---
+++ src/electricity/billing/system/Conn.java
@@ -0,0 +1,32 @@
+
+package electricity.billing.system;
+
+import java.sql.*;
+
+/**
+ * @author souma (SMIT)
+ * @version v1.0
+ * 
+ */
+public class Conn {
+    private Connection c;
+    private Statement s;
+    
+    private final String DATABASE_NAME = "ebs";
+    private final String CONNECTION_URL = "jdbc:mysql:///"+DATABASE_NAME;
+    private final String USER_NAME = "root";
+    private final String PASSWORD = "Soumadip@1";
+    
+    public Conn(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            c = DriverManager.getConnection(CONNECTION_URL,USER_NAME,PASSWORD);
+            s = c.createStatement();
+            System.out.println("Successfully connected with "+DATABASE_NAME+" database...!!!");
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex);
+        }
+        
+    }
+    
+}
