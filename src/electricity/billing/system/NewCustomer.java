16722570511ec624189757892e7effcf4ce96165
Souma <souma.hitech@gmail.com>
2021-01-04 23:13:00 +0530

    rename all component

--- src/electricity/billing/system/NewCustomer.java
+++ src/electricity/billing/system/NewCustomer.java
@@ -1,4 +1,3 @@
-
 package electricity.billing.system;
 
 import java.awt.*;
@@ -12,118 +11,119 @@ import java.util.*;
  * @version v1.0
  *
  */
+public class NewCustomer extends JFrame implements ActionListener {
 
-public class NewCustomer extends JFrame implements ActionListener{
-    JLabel customerNameLabel,meterNoHeadingLabel,addressLabel,stateLabel,cityLabel,emailLabel,phoneNumberLabel,newCustomerImageLabel, meterNoLabel;
-    JTextField customerNameTextField,t2,addressTextField,stateTextField,cityTextField,emailTextField,phoneNumberTextField;
-    JButton nextButton,cancelButton;
-    NewCustomer(){
-        setLocation(600,200);
-        setSize(700,500);
-        
+    JLabel customerNameLabel, meterNoHeadingLabel, addressLabel, stateLabel, cityLabel, emailLabel, phoneNumberLabel, newCustomerImageLabel, meterNoLabel;
+    JTextField customerNameTextField, t2, addressTextField, stateTextField, cityTextField, emailTextField, phoneNumberTextField;
+    JButton nextButton, cancelButton;
+
+    NewCustomer() {
+        setLocation(600, 200);
+        setSize(700, 500);
+
         JPanel panel = new JPanel();
         panel.setLayout(null);
         panel.setBackground(Color.WHITE);
-        panel.setBackground(new Color(173,216,230));
-        
+        panel.setBackground(new Color(173, 216, 230));
+
         JLabel heading = new JLabel("New Customer");
         heading.setBounds(180, 10, 200, 26);
         heading.setFont(new Font("Tahoma", Font.PLAIN, 24));
         panel.add(heading);
-        
+
         customerNameLabel = new JLabel("Customer Name");
         customerNameLabel.setBounds(100, 80, 150, 20);
-        
+
         customerNameTextField = new JTextField();
         customerNameTextField.setBounds(240, 80, 200, 20);
         panel.add(customerNameLabel);
         panel.add(customerNameTextField);
-        
+
         meterNoHeadingLabel = new JLabel("Meter No");
         meterNoHeadingLabel.setBounds(100, 120, 100, 20);
-        
+
         meterNoLabel = new JLabel();
         meterNoLabel.setBounds(240, 120, 200, 20);
         panel.add(meterNoHeadingLabel);
         panel.add(meterNoLabel);
-        
+
         addressLabel = new JLabel("Address");
         addressLabel.setBounds(100, 160, 100, 20);
-        
+
         addressTextField = new JTextField();
         addressTextField.setBounds(240, 160, 200, 20);
         panel.add(addressLabel);
         panel.add(addressTextField);
-        
+
         cityLabel = new JLabel("City");
         cityLabel.setBounds(100, 200, 100, 20);
-        
+
         cityTextField = new JTextField();
         cityTextField.setBounds(240, 200, 200, 20);
         panel.add(cityLabel);
         panel.add(cityTextField);
-        
+
         stateLabel = new JLabel("State");
         stateLabel.setBounds(100, 240, 100, 20);
-        
+
         stateTextField = new JTextField();
         stateTextField.setBounds(240, 240, 200, 20);
         panel.add(stateLabel);
         panel.add(stateTextField);
-        
+
         emailLabel = new JLabel("Email");
         emailLabel.setBounds(100, 280, 100, 20);
-        
+
         emailTextField = new JTextField();
         emailTextField.setBounds(240, 280, 200, 20);
         panel.add(emailLabel);
         panel.add(emailTextField);
-        
+
         phoneNumberLabel = new JLabel("Phone Number");
         phoneNumberLabel.setBounds(100, 320, 100, 20);
-        
+
         phoneNumberTextField = new JTextField();
         phoneNumberTextField.setBounds(240, 320, 200, 20);
         panel.add(phoneNumberLabel);
         panel.add(phoneNumberTextField);
-        
+
         nextButton = new JButton("Next");
         nextButton.setBounds(120, 390, 100, 25);
         nextButton.setBackground(Color.BLACK);
         nextButton.setForeground(Color.WHITE);
-        
+
         cancelButton = new JButton("Cancel");
         cancelButton.setBounds(250, 390, 100, 25);
         cancelButton.setBackground(Color.BLACK);
         cancelButton.setForeground(Color.WHITE);
-        
+
         panel.add(nextButton);
         panel.add(cancelButton);
         setLayout(new BorderLayout());
-        
-        add(panel,"Center");
-        
+
+        add(panel, "Center");
+
         ImageIcon newCustomerIcon = new ImageIcon(NewCustomer.class.getResource("icon/new_customer_icon.jpg"));
-        Image newCustomerImage = newCustomerIcon.getImage().getScaledInstance(150, 300,Image.SCALE_DEFAULT);
+        Image newCustomerImage = newCustomerIcon.getImage().getScaledInstance(150, 300, Image.SCALE_DEFAULT);
         newCustomerIcon = new ImageIcon(newCustomerImage);
         newCustomerImageLabel = new JLabel(newCustomerIcon);
-        add(newCustomerImageLabel,"West");
-        
+        add(newCustomerImageLabel, "West");
+
         //for changing the color of the whole Frame
         getContentPane().setBackground(Color.WHITE);
-        
+
         nextButton.addActionListener(this);
         cancelButton.addActionListener(this);
-        
+
         Random ran = new Random();
         long first = (ran.nextLong() % 1000000);
-        meterNoLabel.setText(""+Math.abs(first));
-        
+        meterNoLabel.setText("" + Math.abs(first));
+
     }
-    
+
     @Override
-    public void actionPerformed(ActionEvent events){
-        if(events.getSource() == nextButton){
+    public void actionPerformed(ActionEvent events) {
+        if (events.getSource() == nextButton) {
             String name = customerNameTextField.getText().trim();
             String meter = meterNoLabel.getText().trim();
             String address = addressTextField.getText().trim();
@@ -132,27 +132,26 @@ public class NewCustomer extends JFrame implements ActionListener{
             String email = emailTextField.getText().trim();
             String phone = phoneNumberTextField.getText().trim();
 
-            String query1 = "insert into customer values('"+name+"','"+meter+"','"+address+"','"+city+"','"+state+"','"+email+"','"+phone+"')";
-            String query2 = "insert into login values('"+meter+"', '', '', '', '')";
-            
-            try{
+            String query1 = "insert into customer values('" + name + "','" + meter + "','" + address + "','" + city + "','" + state + "','" + email + "','" + phone + "')";
+            String query2 = "insert into login values('" + meter + "', '', '', '', '')";
+
+            try {
                 Conn connection = new Conn();
                 connection.statement.executeUpdate(query1);
                 connection.statement.executeUpdate(query2);
-                JOptionPane.showMessageDialog(null,"Customer Details Added Successfully");
+                JOptionPane.showMessageDialog(null, "Customer Details Added Successfully");
                 this.setVisible(false);
                 //new MeterInfo(meter).setVisible(true);
 
-            }catch(Exception ex){
-                 ex.printStackTrace();
+            } catch (HeadlessException | SQLException ex) {
+                ex.printStackTrace();
             }
-        }else if(events.getSource() ==cancelButton){
-                this.setVisible(false);
-                }
+        } else if (events.getSource() == cancelButton) {
+            this.dispose();
+        }
     }
-    
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         new NewCustomer().setVisible(true);
     }
 }
